cf7d905d4978ab79f956823847fed0b5506296f2
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-11-03 20:47:57 +0200

    update

---
+++ Inheritance/Inheritance Lab Problems/P04_Random_List/RandomList.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            var curr = new Random();
+            var index = curr.Next(0, this.Count);
+            var element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
---
+++ Inheritance/Inheritance Lab Problems/P04_Random_List/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomRandomList
+{
+    using System;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
