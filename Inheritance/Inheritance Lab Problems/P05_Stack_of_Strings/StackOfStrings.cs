b3fe16e1e98d80827a615adb6d9f638523d55788
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-11-03 21:46:35 +0200

    update

---
+++ Inheritance/Inheritance Lab Problems/P05_Stack_of_Strings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿namespace CustomStack
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return base.Count.Equals(0);
+        }
+
+        public void AddRange(Stack<string> stackAdd)
+        {
+            while (stackAdd.Count > 0)
+            {
+                base.Push(stackAdd.Pop());
+            }
+        }
+    }
+}
