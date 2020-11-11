143486fcb7031136ae65e7fcadecea97240b30bc
Daniel Stoyanov <dan.stoyanov@abv.bg>
2020-11-11 12:15:03 +0200

    update

---
+++ Encapsulation/Encapsulation Lab Problems/P01_Persons/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public Person(string fistName, string lastName, int age)
+            : this()
+        {
+            this.FirstName = fistName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public int Age { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
