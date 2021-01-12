68aeb6411725f78650990a41433c8584682f6c14
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-01-12 17:08:47 +0200

    update

---
+++ Inheritance/Inheritance Exercise Problems/P06_Animals/Animal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private const string ERROR_MESSAGE = "Invalid input!";
+
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ERROR_MESSAGE);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ERROR_MESSAGE);
+                }
+
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            protected set
+            {
+                if (value != "Female" && value != "Male")
+                {
+                    throw new ArgumentException(ERROR_MESSAGE);
+                }
+
+                this.gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"{this.GetType().Name}");
+            stringBuilder.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            stringBuilder.AppendLine($"{this.ProduceSound()}");
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Inheritance/Inheritance Exercise Problems/P06_Animals/Engine.cs
@@ -0,0 +1,103 @@
+﻿using Animals.Cats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Animals
+{
+    public class Engine
+    {
+        private const string END_OF_INPUT_COMMAND = "Beast!";
+        private readonly List<Animal> animals;
+
+        public Engine()
+        {
+            this.animals = new List<Animal>();
+        }
+
+        public void Run()
+        {
+            string type;
+
+            while ((type = Console.ReadLine()) != END_OF_INPUT_COMMAND)
+            {
+                var commArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                Animal animal;
+
+                try
+                {
+                    animal = GetAnimal(type, commArgs);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                this.animals.Add(animal);
+            }
+
+            PrintOutput();
+        }
+
+        private void PrintOutput()
+        {
+            foreach (Animal animal in this.animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+
+        private Animal GetAnimal(string type, string[] commArgs)
+        {
+            var name = commArgs[0];
+            var age = int.Parse(commArgs[1]);
+            string gender = GetGender(commArgs);
+
+            Animal animal = null;
+
+            switch (type)
+            {
+                case "Dog":
+                    animal = new Dog(name, age, gender);
+                    break;
+
+                case "Cat":
+                    animal = new Cat(name, age, gender);
+                    break;
+
+                case "Frog":
+                    animal = new Frog(name, age, gender);
+                    break;
+
+                case "Kitten":
+                    animal = new Kitten(name, age);
+                    break;
+
+                case "Tomcat":
+                    animal = new Tomcat(name, age);
+                    break;
+
+                default:
+                    throw new ArgumentException("Invalid input!");
+            }
+
+            return animal;
+        }
+
+        private string GetGender(string[] commArgs)
+        {
+            string gender = null;
+
+            if (commArgs.Length > 2)
+            {
+                gender = commArgs[2];
+            }
+
+            return gender;
+        }
+    }
+}
