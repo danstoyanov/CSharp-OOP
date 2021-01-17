8fe6fc0493e1b2b1048411fe9def5147b88963d1
danstoyanov <dan.stoyanov@abv.bg>
2021-01-17 18:18:57 +0200

    update

---
+++ Encapsulation/Encapsulation Exercise Problems/P03_Shopping_Spree/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03_Shopping_Spree
+{
+    public class Product
+    {
+        private const double MIN_COST_VALUE = 0;
+
+        private string name;
+        private double money;
+
+        public Product(string name, double money)
+        {
+            this.Name = name;
+            this.Money = money;
+        }
+
+       public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public double Money
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < MIN_COST_VALUE)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        // Bag with Products !!! 
+    }
+}
---
+++ Encapsulation/Encapsulation Exercise Problems/P03_Shopping_Spree/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03_Shopping_Spree
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+           
+        }
+    }
+}
