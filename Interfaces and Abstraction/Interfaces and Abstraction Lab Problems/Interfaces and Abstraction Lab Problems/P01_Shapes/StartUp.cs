91eb7a50784beea8eef2efaa9c9d8ac7b47048c8
danstoyanov <dan.stoyanov@abv.bg>
2021-01-22 15:24:22 +0200

    update

---
+++ Interfaces and Abstraction/Interfaces and Abstraction Lab Problems/Interfaces and Abstraction Lab Problems/P01_Shapes/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_Shapes
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ Interfaces and Abstraction/Interfaces and Abstraction Lab Problems/Interfaces and Abstraction Lab Problems/P02_Cars/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02_Cars
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
