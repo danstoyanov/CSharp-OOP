7928c1b2dbe6024492d2c29a7db90aa3b64ac47a
danstoyanov <dan.stoyanov@abv.bg>
2021-01-24 22:55:55 +0200

    update

---
+++ Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/Interfaces and Abstraction Exercise Problems/Interfaces and Abstraction Exercise Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interfaces_and_Abstraction_Exercise_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
