9975d2a72ae685a2e07526a8cbe8c9412f49cf2f
danstoyanov <dan.stoyanov@abv.bg>
2021-01-30 13:27:45 +0200

    update

--- Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/P06_Food_Shortage/StartUp.cs
+++ Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/P06_Food_Shortage/StartUp.cs
@@ -2,11 +2,11 @@
 
 namespace P06_Food_Shortage
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
--- Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/P09_Explicit_Interfaces/StartUp.cs
+++ Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/P09_Explicit_Interfaces/StartUp.cs
@@ -2,11 +2,11 @@
 
 namespace P09_Explicit_Interfaces
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
