be50206e69b5413ac5111ca900e8f4de7e82f608
danstoyanov <dan.stoyanov@abv.bg>
2021-02-03 18:09:15 +0200

    update

--- Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/P08_Collection_Hierarchy/StartUp.cs
+++ Interfaces and Abstraction/Interfaces and Abstraction Exercise Problems/P08_Collection_Hierarchy/StartUp.cs
@@ -2,11 +2,11 @@
 
 namespace P08_Collection_Hierarchy
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
