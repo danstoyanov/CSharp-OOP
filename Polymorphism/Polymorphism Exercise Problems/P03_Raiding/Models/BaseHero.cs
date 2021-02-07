4bc315357faee2a64c8517a3d59ca4cbb39d3149
danstoyanov <dan.stoyanov@abv.bg>
2021-02-08 00:01:13 +0200

    update

--- Polymorphism/Polymorphism Exercise Problems/P03_Raiding/Models/BaseHero.cs
+++ Polymorphism/Polymorphism Exercise Problems/P03_Raiding/Models/BaseHero.cs
@@ -4,6 +4,12 @@ namespace P03_Raiding.Models
 {
     public class BaseHero : IBaseHero
     {
+        public BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+
         public string Name { get; private set; }
 
         public int Power { get; private set; }
