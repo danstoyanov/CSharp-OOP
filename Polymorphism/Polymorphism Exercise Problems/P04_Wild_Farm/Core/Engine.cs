aab0eeca3f58454d6c27f45dffeae0ca810ecefa
danstoyanov <dan.stoyanov@abv.bg>
2021-02-08 17:28:15 +0200

    update

---
+++ Polymorphism/Polymorphism Exercise Problems/P04_Wild_Farm/Core/Engine.cs
@@ -0,0 +1,15 @@
+﻿namespace P04_Wild_Farm.Core
+{
+    public class Engine
+    {
+        public Engine()
+        {
+
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}
--- Polymorphism/Polymorphism Exercise Problems/P04_Wild_Farm/StartUp.cs
+++ Polymorphism/Polymorphism Exercise Problems/P04_Wild_Farm/StartUp.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
+using P04_Wild_Farm.Core;
+
 namespace P04_Wild_Farm
 {
     public class StartUp
     {
         static void Main()
         {
-
+            Engine engine = new Engine();
+            engine.Run();
         }
     }
 }
