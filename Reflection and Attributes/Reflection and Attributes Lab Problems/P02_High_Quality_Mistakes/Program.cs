a5d7356ad3dd40750bae9553fde94bf13f99247c
danstoyanov <dan.stoyanov@abv.bg>
2021-02-12 13:35:20 +0200

    update

---
+++ Reflection and Attributes/Reflection and Attributes Lab Problems/P02_High_Quality_Mistakes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02_High_Quality_Mistakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
