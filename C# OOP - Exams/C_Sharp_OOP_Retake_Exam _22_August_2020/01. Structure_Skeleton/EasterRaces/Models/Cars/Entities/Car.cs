e18313702658fc3e62c74980f1347a9b4ff1fbeb
danstoyanov <dan.stoyanov@abv.bg>
2021-02-19 13:10:30 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Retake_Exam _22_August_2020/01. Structure_Skeleton/EasterRaces/Models/Cars/Entities/Car.cs
@@ -0,0 +1,25 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    public abstract class Car : ICar
+    {
+        public Car(string model, int horsePower, double cubicCentimeters)
+        {
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.CubicCentimeters = cubicCentimeters;
+        }
+
+        public string Model { get; private set; }
+
+        public int HorsePower { get; private set; }
+
+        public double CubicCentimeters { get; private set; }
+
+        public double CalculateRacePoints(int laps)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
