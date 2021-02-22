8da4b252da2fdcaba1ab889514e4143b82f6825e
danstoyanov <dan.stoyanov@abv.bg>
2021-02-22 16:35:31 +0200

    update

---
+++ Reflection and Attributes/Reflection and Attributes Lab Problems/P01_Stealer/ISpy.cs
@@ -0,0 +1,7 @@
+﻿namespace Stealer
+{
+    public interface ISpy
+    {
+        string StealFieldInfo(string investigate, params string[] arr);
+    }
+}
--- Reflection and Attributes/Reflection and Attributes Lab Problems/P01_Stealer/StartUp.cs
+++ Reflection and Attributes/Reflection and Attributes Lab Problems/P01_Stealer/StartUp.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 
-namespace P01_Stealer
+namespace Stealer
 {
     public class StartUp
     {
         static void Main()
         {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+            Console.WriteLine(result.ToString());
 
         }
     }
