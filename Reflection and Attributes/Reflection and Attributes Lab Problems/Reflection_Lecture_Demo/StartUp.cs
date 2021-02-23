d4a739b5575b74dc8712e699db9cca6083145e30
danstoyanov <dan.stoyanov@abv.bg>
2021-02-23 15:03:23 +0200

    update

---
+++ Reflection and Attributes/Reflection and Attributes Lab Problems/Reflection_Lecture_Demo/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_Lecture_Demo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var testType = Type.GetType("Reflection_Lecture_Demo.TestClass");
+
+            // how to get the Type of current Class
+            TestClass test = (TestClass)Activator.CreateInstance(testType, new object[] { "Pesho", 23 });
+
+
+            // how to get fields from current Class
+            FieldInfo field = testType.GetField("secret",
+                BindingFlags.NonPublic |
+                BindingFlags.Instance);
+
+            var printInfo = field.Name;
+
+            // how to get all Constructors from current Class
+            ConstructorInfo[] cinInfo = testType.GetConstructors();
+
+
+            // how to get all Methods from current Class
+
+            TestClass newInstance = (TestClass)cinInfo[1].Invoke(new object[] { "Mario" });
+
+            MethodInfo[] methods = testType.GetMethods();
+
+            // how to get only one Method from the current Class
+            MethodInfo who = testType.GetMethod("WhoAmI");
+
+            // Invoke 
+            who.Invoke(newInstance, null);
+
+            field.SetValue(test, "");
+
+
+
+        }
+    }
+}
---
+++ Reflection and Attributes/Reflection and Attributes Lab Problems/Reflection_Lecture_Demo/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Reflection_Lecture_Demo
+{
+    [Author("Galin")]
+
+    public class TestClass
+    {
+        private string secret = "MySecret";
+        private bool isCreated = false;
+        public string name;
+
+        public TestClass()
+        {
+            isCreated = true;
+        }
+
+        public TestClass(string name)
+            : this()
+        {
+            this.name = name;
+        }
+
+        public TestClass(string name, int age)
+           : this(name)
+        {
+            this.Age = age;
+        }
+
+        public int Age { get; set; }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine(name);
+        }
+
+        public void TellMeASecret()
+        {
+            Console.WriteLine(secret);
+        }
+    }
+}
