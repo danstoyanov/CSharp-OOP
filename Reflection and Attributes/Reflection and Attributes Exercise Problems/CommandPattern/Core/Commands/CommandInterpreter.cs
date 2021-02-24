859a300bfff8291ff91262894fce55bfe0a3d52b
danstoyanov <dan.stoyanov@abv.bg>
2021-02-24 13:31:05 +0200

    update

---
+++ Reflection and Attributes/Reflection and Attributes Exercise Problems/CommandPattern/Core/Commands/CommandInterpreter.cs
@@ -0,0 +1,19 @@
+﻿using CommandPattern.Core.Contracts;
+using System.Linq;
+
+namespace CommandPattern.Core.Commands
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var strArr = args
+                .Split()
+                .ToArray();
+
+
+
+            return null;
+        }
+    }
+}
