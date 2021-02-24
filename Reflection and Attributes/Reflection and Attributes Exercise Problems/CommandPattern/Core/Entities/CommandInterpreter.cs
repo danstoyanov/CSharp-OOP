91c936c564d8dfb87478be6cc39550181eb70193
danstoyanov <dan.stoyanov@abv.bg>
2021-02-24 15:37:30 +0200

    update

--- Reflection and Attributes/Reflection and Attributes Exercise Problems/CommandPattern/Core/Entities/CommandInterpreter.cs
+++ Reflection and Attributes/Reflection and Attributes Exercise Problems/CommandPattern/Core/Entities/CommandInterpreter.cs
@@ -1,39 +1,55 @@
 ﻿using System;
 using System.Linq;
-
+using System.Reflection;
 using CommandPattern.Core.Contracts;
 
 namespace CommandPattern.Core.Commands
 {
     public class CommandInterpreter : ICommandInterpreter
     {
+        private const string CommandPostFix = "Command";
+
         public string Read(string args)
         {
             string[] tokens = args
                 .Split()
                 .ToArray();
+
+
             string commandName = tokens[0];
+            string commandTypeName = commandName + CommandPostFix;
 
-            ICommand command = null;
+            // *NEW = Reflection, how to get type !
+            Type commandType = Assembly
+                .GetCallingAssembly()
+                .GetTypes()
+                .Where(t => t.GetInterfaces().Any(i => i.Name == nameof(ICommand))) // <= Get all interfaces only with ICommand !
+                .FirstOrDefault(t => t.Name == commandTypeName);
 
-            if (commandName == "Hello")
+            if (commandType == null)
             {
-                command = new HelloCommand();
-            }
-            else if (commandName == "Exit")
-            {
-                command = new ExitCommand();
-            }
-            else
-            {
-                throw new InvalidOperationException("Invalid command name!");
+                throw new InvalidOperationException("Command type is invalid!");
             }
 
+            // *NEW When commandType != null, we must create the object from current command type !
+            ICommand command = Activator.CreateInstance(commandType) as ICommand;
 
             string[] readData = tokens.Skip(1).ToArray();
             string result = command.Execute(readData);
-            
+
             return result;
+            //if (commandName == "Hello")
+            //{
+            //    command = new HelloCommand();
+            //}
+            //else if (commandName == "Exit")
+            //{
+            //    command = new ExitCommand();
+            //}
+            //else
+            //{
+            //    throw new InvalidOperationException("Invalid command name!");
+            //}
         }
     }
 }
