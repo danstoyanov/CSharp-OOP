c80a65e276424d3dc6891af04fcebcd45a560370
danstoyanov <dan.stoyanov@abv.bg>
2021-02-24 14:15:32 +0200

    update

---
+++ Reflection and Attributes/Reflection and Attributes Exercise Problems/CommandPattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,14 @@
+﻿
+
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
