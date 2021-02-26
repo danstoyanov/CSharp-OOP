f268b35cb8e3720ade8a6dcc8d7c6c1060ae46f4
danstoyanov <dan.stoyanov@abv.bg>
2021-02-26 11:52:24 +0200

    update

--- Unit Testing/Unit Testing Lab Problems/P03.MyTestDemos.Tests/CalculatorTest.cs
+++ Unit Testing/Unit Testing Lab Problems/P03.MyTestDemos.Tests/CalculatorTest.cs
@@ -1,7 +1,12 @@
+using Xunit;
+
 namespace P03.MyTestDemos.Tests
 {
     public class CalculatorTest
     {
+        public void SumShouldReturnCorrectResultWithTwoNumbers()
+        {
 
+        }
     }
 }
