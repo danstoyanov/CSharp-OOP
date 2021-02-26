a6908dab219512588a0f7747f4a4e8b1e99efe2a
danstoyanov <dan.stoyanov@abv.bg>
2021-02-26 20:03:36 +0200

    update

---
+++ Unit Testing/Unit Testing Exercise Problems/P01_Database_Tests/DatabaseTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [Test]
+        public void If_Array_Count_Are_Equal_Of_Sixteen_Integers()
+        {
+            // Arrenge
+            var array = new int[16];
+            Database dadadadadadadad = new Database();
+
+            // Act
+
+            // Asserts
+        }
+    }
+}
