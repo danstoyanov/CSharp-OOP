803ac412c2f91ef3e6cd7f349961aa66c78fdf1d
danstoyanov <dan.stoyanov@abv.bg>
2021-02-26 19:41:21 +0200

    update

---
+++ Unit Testing/Unit Testing Exercise Problems/P02_Extended_Database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02_Extended_Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Unit Testing/Unit Testing Exercise Problems/P04_Fighting_Arena/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P04_Fighting_Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
