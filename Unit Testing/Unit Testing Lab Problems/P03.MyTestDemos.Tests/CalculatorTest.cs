23f8eb76d1f4764904ac195f3371bfe8ae44a95e
danstoyanov <dan.stoyanov@abv.bg>
2021-02-26 11:58:16 +0200

    update

--- Unit Testing/Unit Testing Lab Problems/P03.MyTestDemos.Tests/CalculatorTest.cs
+++ Unit Testing/Unit Testing Lab Problems/P03.MyTestDemos.Tests/CalculatorTest.cs
@@ -1,4 +1,7 @@
 using Xunit;
+using System;
+
+using P03MyTestDemos.App;
 
 namespace P03.MyTestDemos.Tests
 {
@@ -6,7 +9,37 @@ namespace P03.MyTestDemos.Tests
     {
         public void SumShouldReturnCorrectResultWithTwoNumbers()
         {
+            Calculator calculator = new Calculator();
 
+            var result = calculator.Sum(1, 2);
+
+            if (result != 3)
+            {
+                throw new InvalidOperationException("SumShouldReturnCorrectResultWithTwoNumbers failed !");
+            }
+        }
+
+        public void SumShouldReturnCorrectResultWithManyNumbers()
+        {
+            Calculator calculator = new Calculator();
+
+            var result = calculator.Sum(1, 2, 3, 4, 5, 6);
+
+            if (result != 21)
+            {
+                throw new InvalidOperationException("SumShouldReturnCorrectResultWithTwoNumbers failed !");
+            }
+        }
+        public void SumShouldReturnCorrectResultWithNoNumbers()
+        {
+            Calculator calculator = new Calculator();
+
+            var result = calculator.Sum();
+
+            if (result != 0)
+            {
+                throw new InvalidOperationException("SumShouldReturnCorrectResultWithTwoNumbers failed !");
+            }
         }
     }
 }
