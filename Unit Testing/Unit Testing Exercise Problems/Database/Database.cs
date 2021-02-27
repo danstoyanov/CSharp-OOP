9e88706fc88c31583683bce7aa8165f6340159c3
danstoyanov <dan.stoyanov@abv.bg>
2021-02-27 11:29:46 +0200

    update

--- Unit Testing/Unit Testing Exercise Problems/Database/Database.cs
+++ Unit Testing/Unit Testing Exercise Problems/Database/Database.cs
@@ -1,62 +1,59 @@
 ﻿using System;
 
-namespace Database
+public class Database
 {
-    public class Database
+    private int[] data;
+
+    private int count;
+
+    public Database(params int[] data)
     {
-        private int[] data;
+        this.data = new int[16];
 
-        private int count;
-
-        public Database(params int[] data)
+        for (int i = 0; i < data.Length; i++)
         {
-            this.data = new int[16];
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                this.Add(data[i]);
-            }
-
-            this.count = data.Length;
+            this.Add(data[i]);
         }
 
-        public int Count
+        this.count = data.Length;
+    }
+
+    public int Count
+    {
+        get { return count; }
+    }
+
+    public void Add(int element)
+    {
+        if (this.count == 16)
         {
-            get { return count; }
+            throw new InvalidOperationException("Array's capacity must be exactly 16 integers!");
         }
 
-        public void Add(int element)
-        {
-            if (this.count == 16)
-            {
-                throw new InvalidOperationException("Array's capacity must be exactly 16 integers!");
-            }
+        this.data[this.count] = element;
+        this.count++;
+    }
 
-            this.data[this.count] = element;
-            this.count++;
+    public void Remove()
+    {
+        if (this.count == 0)
+        {
+            throw new InvalidOperationException("The collection is empty!");
         }
 
-        public void Remove()
-        {
-            if (this.count == 0)
-            {
-                throw new InvalidOperationException("The collection is empty!");
-            }
+        this.count--;
+        this.data[this.count] = 0;
+    }
 
-            this.count--;
-            this.data[this.count] = 0;
+    public int[] Fetch()
+    {
+        int[] coppyArray = new int[this.count];
+
+        for (int i = 0; i < this.count; i++)
+        {
+            coppyArray[i] = this.data[i];
         }
 
-        public int[] Fetch()
-        {
-            int[] coppyArray = new int[this.count];
-
-            for (int i = 0; i < this.count; i++)
-            {
-                coppyArray[i] = this.data[i];
-            }
-
-            return coppyArray;
-        }
+        return coppyArray;
     }
 }
