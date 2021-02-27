7ee6e2e5483f51f0f4a69b91fdb9cd7a10ffc954
danstoyanov <dan.stoyanov@abv.bg>
2021-02-27 11:30:02 +0200

    update

--- Unit Testing/Unit Testing Exercise Problems/Database.Tests/DatabaseTests.cs
+++ Unit Testing/Unit Testing Exercise Problems/Database.Tests/DatabaseTests.cs
@@ -2,13 +2,44 @@ using NUnit.Framework;
 
 namespace Tests
 {
+    [TestFixture]
     public class DatabaseTests
     {
+        private int[] testArray = new int[16];
+
         [SetUp]
         public void Setup()
         {
         }
 
+        [Test]
+        public void Capacity_Test_Are_Equal_To_Sixteen()
+        {
+            // Arrange
+            var data = new Database(this.testArray);
 
+            // Act
+            var arrSize = this.testArray.Length;
+
+            // Assert
+            Assert.AreEqual(data.Count, arrSize);
+        }
+
+        [Test]
+        public void Try_To_Add_Element_At_The_Free_Cell()
+        {
+            // Arrange
+            const int ADD_VALUE = 23;
+
+            var data = new Database(this.testArray);
+
+            // Assert
+            Assert.That(() => data.Add(ADD_VALUE),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("Array's capacity must be exactly 16 integers!"));
+        }
     }
 }
