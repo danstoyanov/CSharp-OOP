956297f143eaec5bbee8d790370c8719989c5115
danstoyanov <dan.stoyanov@abv.bg>
2021-03-02 18:11:57 +0200

    update

--- Unit Testing/Unit Testing Exercise Problems/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ Unit Testing/Unit Testing Exercise Problems/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using ExtendedDatabase;
 using NUnit.Framework;
 
@@ -15,20 +16,18 @@ namespace Tests
         public void User_With_Same_Username_Exception()
         {
             //Arrange
-            long firstID = 54932233;
-            string firstName = "Mincho";
-
-            long secondID = 323132158;
-            string secondName = "Ivan";
-
-            var firstPerson = new Person(firstID, firstName);
-            var secondPerson = new Person(secondID, secondName);
-
-            var data = new ExtendedDatabase.ExtendedDatabase(firstPerson, secondPerson);
+            var testPerson = new Person(31313131, "Gesha");
+            var data = new ExtendedDatabase(new Person(31313131, "Gesha"));
 
             //Act
 
             //Assert
+            Assert.That(() => data.Add(TEST_PERSON),
+                Throws
+                .InvalidOperationException
+                .With
+                .Message
+                .EqualTo("There is already user with this username!"));
         }
     }
 }
