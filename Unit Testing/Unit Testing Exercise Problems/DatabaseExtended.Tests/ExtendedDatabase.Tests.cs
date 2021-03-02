2cd45c9de5260509cef9ce2105995fbe08cb9b7a
danstoyanov <dan.stoyanov@abv.bg>
2021-03-02 15:41:58 +0200

    update

--- Unit Testing/Unit Testing Exercise Problems/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ Unit Testing/Unit Testing Exercise Problems/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,3 +1,4 @@
+using ExtendedDatabase;
 using NUnit.Framework;
 
 namespace Tests
@@ -8,6 +9,26 @@ namespace Tests
         public void Setup()
         {
 
+        }
+
+        [Test]
+        public void User_With_Same_Username_Exception()
+        {
+            //Arrange
+            long firstID = 54932233;
+            string firstName = "Mincho";
+
+            long secondID = 323132158;
+            string secondName = "Ivan";
+
+            var firstPerson = new Person(firstID, firstName);
+            var secondPerson = new Person(secondID, secondName);
+
+            var data = new ExtendedDatabase.ExtendedDatabase(firstPerson, secondPerson);
+
+            //Act
+
+            //Assert
         }
     }
 }
---
+++ Unit Testing/Unit Testing Exercise Problems/Demo_Tests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Demo_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[16];
+
+            var data = new Database();
+
+            ;
+
+            data.Add(41);
+            data.Add(32);
+
+            data.Remove();
+            data.Remove();
+
+            var currentValue = data.Count;
+
+        }
+    }
+}
