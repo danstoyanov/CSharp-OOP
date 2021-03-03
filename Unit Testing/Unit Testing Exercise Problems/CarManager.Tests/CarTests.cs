a6606a2cf50403776fc8cefb3a3db587aa254151
danstoyanov <dan.stoyanov@abv.bg>
2021-03-03 12:42:22 +0200

    update

--- Unit Testing/Unit Testing Exercise Problems/CarManager.Tests/CarTests.cs
+++ Unit Testing/Unit Testing Exercise Problems/CarManager.Tests/CarTests.cs
@@ -1,3 +1,4 @@
+using CarManager;
 using NUnit.Framework;
 
 namespace Tests
@@ -8,6 +9,21 @@ namespace Tests
         public void Setup()
         {
 
+        }
+
+        [Test]
+        public void Test_Car_Class_Implementation_Correctly()
+        {
+            // Arrange
+            string make = "Toyota";
+            string model = "Corolla";
+            double fuelConsimption = 4.50;
+            double fuelCapacity = 200;
+
+            var car = new Car(make, model, fuelConsimption, fuelCapacity);
+            // Act
+
+            // Assert
         }
     }
 }
