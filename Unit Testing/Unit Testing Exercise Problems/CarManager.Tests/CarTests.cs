2ac779b7534838229212c916ba1128b85ad1f581
danstoyanov <dan.stoyanov@abv.bg>
2021-03-03 12:57:43 +0200

    update

--- Unit Testing/Unit Testing Exercise Problems/CarManager.Tests/CarTests.cs
+++ Unit Testing/Unit Testing Exercise Problems/CarManager.Tests/CarTests.cs
@@ -1,10 +1,17 @@
-using CarManager;
+using System;
 using NUnit.Framework;
+
+using CarManager;
 
 namespace Tests
 {
     public class CarTests
     {
+        private const string make = "Toyota";
+        private const string model = "Corolla";
+        private const double fuelConsimption = 4.50;
+        private const double fuelCapacity = 200;
+
         [SetUp]
         public void Setup()
         {
@@ -15,15 +22,24 @@ namespace Tests
         public void Test_Car_Class_Implementation_Correctly()
         {
             // Arrange
-            string make = "Toyota";
-            string model = "Corolla";
-            double fuelConsimption = 4.50;
-            double fuelCapacity = 200;
-
             var car = new Car(make, model, fuelConsimption, fuelCapacity);
-            // Act
 
             // Assert
+            Assert.AreEqual(make, car.Make);
+            Assert.AreEqual(model, car.Model);
+            Assert.AreEqual(fuelConsimption, car.FuelConsumption);
+            Assert.AreEqual(fuelCapacity, car.FuelCapacity);
+        }
+
+        [Test]
+        public void Test_Make_If_Is_Null_Or_Empty_And_Throw_Exception_MSG()
+        {
+            // Arrange
+            var car = new Car(make, model, fuelConsimption, fuelCapacity);
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            car = new Car("", model, fuelConsimption, fuelCapacity), "Make cannot be null or empty!");
         }
     }
 }
