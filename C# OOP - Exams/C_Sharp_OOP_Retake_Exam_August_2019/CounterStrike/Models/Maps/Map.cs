8446788fca04f3dcde26982310c5363aa289c625
danstoyanov <dan.stoyanov@abv.bg>
2021-03-10 20:39:43 +0200

    update

--- C# OOP - Exams/C_Sharp_OOP_Retake_Exam_August_2019/CounterStrike/Models/Maps/Map.cs
+++ C# OOP - Exams/C_Sharp_OOP_Retake_Exam_August_2019/CounterStrike/Models/Maps/Map.cs
@@ -6,8 +6,16 @@ using CounterStrike.Models.Players.Contracts;
 namespace CounterStrike.Models.Maps
 {
     public class Map : IMap
-
     {
+        private List<IPlayer> terrorosits;
+        private List<IPlayer> counterTerrorists;
+
+        public Map()
+        {
+            this.terrorosits = new List<IPlayer>();
+            this.counterTerrorists = new List<IPlayer>();
+        }
+
         public string Start(ICollection<IPlayer> players)
         {
             throw new System.NotImplementedException();
