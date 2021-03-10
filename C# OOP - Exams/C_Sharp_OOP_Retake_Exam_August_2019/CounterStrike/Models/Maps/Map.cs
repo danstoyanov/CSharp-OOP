b39b52c133e4580973eea0e6a910edd599a77413
danstoyanov <dan.stoyanov@abv.bg>
2021-03-10 20:32:40 +0200

    update

--- C# OOP - Exams/C_Sharp_OOP_Retake_Exam_August_2019/CounterStrike/Models/Maps/Map.cs
+++ C# OOP - Exams/C_Sharp_OOP_Retake_Exam_August_2019/CounterStrike/Models/Maps/Map.cs
@@ -1,8 +1,16 @@
-﻿namespace CounterStrike.Models.Maps
+﻿using System.Collections.Generic;
+
+using CounterStrike.Models.Maps.Contracts;
+using CounterStrike.Models.Players.Contracts;
+
+namespace CounterStrike.Models.Maps
 {
     public class Map : IMap
 
     {
-
+        public string Start(ICollection<IPlayer> players)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
