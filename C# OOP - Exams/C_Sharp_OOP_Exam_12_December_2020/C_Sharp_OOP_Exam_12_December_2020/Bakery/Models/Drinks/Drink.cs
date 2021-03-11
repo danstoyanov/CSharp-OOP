2e95550a4a4684cea4dc87c8d77edd203c3d7637
danstoyanov <dan.stoyanov@abv.bg>
2021-03-11 15:13:22 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Exam_12_December_2020/C_Sharp_OOP_Exam_12_December_2020/Bakery/Models/Drinks/Drink.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Bakery.Utilities.Messages;
+using Bakery.Models.Drinks.Contracts;
+
+namespace Bakery.Models.Drinks
+{
+    public abstract class Drink : IDrink
+    {
+        private string name;
+        private int portion;
+        private decimal price;
+        private string brand;
+
+        public Drink(string name, int portion, decimal price, string brand)
+        {
+            this.Name = name;
+            this.Portion = portion;
+            this.Price = price;
+            this.Brand = brand;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName));
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Portion
+        {
+            get
+            {
+                return this.portion;
+            }
+            protected set
+            {
+
+            }
+        }
+
+        public decimal Price => throw new System.NotImplementedException();
+
+        public string Brand => throw new System.NotImplementedException();
+    }
+}
