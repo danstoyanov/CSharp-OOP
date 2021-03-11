bae785c2f49b200de686761aba93b008233fb2ef
danstoyanov <dan.stoyanov@abv.bg>
2021-03-11 12:29:44 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Retake_Exam_August_2019/CounterStrike/Repositories/GunRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Repositories.Contracts;
+
+namespace CounterStrike.Repositories
+{
+    public class GunRepository : IRepository<IGun>
+    {
+        public IReadOnlyCollection<IGun> Models => throw new System.NotImplementedException();
+
+        public void Add(IGun model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IGun FindByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(IGun model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
