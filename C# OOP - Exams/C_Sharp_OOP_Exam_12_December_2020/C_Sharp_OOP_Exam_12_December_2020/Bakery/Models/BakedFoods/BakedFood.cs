fce6c11a416d088e88712e8ffe778f4759380f4a
danstoyanov <dan.stoyanov@abv.bg>
2021-03-11 14:51:59 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Exam_12_December_2020/C_Sharp_OOP_Exam_12_December_2020/Bakery/Models/BakedFoods/BakedFood.cs
@@ -0,0 +1,76 @@
+﻿using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Utilities.Messages;
+using System;
+
+namespace Bakery
+{
+    public abstract class BakedFood : IBakedFood
+    {
+        private string name;
+        private int portion;
+        private decimal price;
+
+        public BakedFood(string name, int portion, decimal price)
+        {
+            this.Name = name;
+            this.Portion = portion;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName));
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Portion
+        {
+            get
+            {
+                return this.portion;
+            }
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPortion));
+                }
+
+                this.portion = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidPrice));
+                }
+
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name}: {this.Portion}g - {this.Portion:F2}";
+        }
+    }
+}
