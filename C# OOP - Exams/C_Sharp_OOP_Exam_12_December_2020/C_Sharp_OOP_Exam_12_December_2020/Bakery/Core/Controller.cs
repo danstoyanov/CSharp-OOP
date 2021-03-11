018cc1b0f5c3538232debcfe87df711627f171ed
danstoyanov <dan.stoyanov@abv.bg>
2021-03-11 15:05:53 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Exam_12_December_2020/C_Sharp_OOP_Exam_12_December_2020/Bakery/Core/Controller.cs
@@ -0,0 +1,50 @@
+﻿namespace Bakery.Core.Contracts
+{
+    public class Controller : IController
+    {
+        public string AddDrink(string type, string name, int portion, string brand)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddFood(string type, string name, decimal price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddTable(string type, int tableNumber, int capacity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetFreeTablesInfo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetTotalIncome()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string LeaveTable(int tableNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string OrderFood(int tableNumber, string foodName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ReserveTable(int numberOfPeople)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
