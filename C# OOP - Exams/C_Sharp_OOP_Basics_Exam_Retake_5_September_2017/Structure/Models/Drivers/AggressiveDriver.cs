007e314bbc2786bf7d7cb3a1f5d6d69910b8f2e2
danstoyanov <dan.stoyanov@abv.bg>
2021-03-13 17:07:40 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Basics_Exam_Retake_5_September_2017/Structure/Models/Drivers/AggressiveDriver.cs
@@ -0,0 +1,17 @@
+﻿using Structure.Models.Cars;
+
+namespace Structure.Models.Drivers
+{
+    public class AggressiveDriver : Driver
+    {
+        private const double AGRESIVE_FUEL_CONSP_PER_KM = 2.7;
+
+        public AggressiveDriver(string name, double totalTime, Car car) 
+            : base(name, totalTime, car, AGRESIVE_FUEL_CONSP_PER_KM)
+        {
+
+        }
+
+        public override double Speed => base.Speed * 1.3;
+    }
+}
