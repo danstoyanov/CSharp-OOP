15d5d7cf936b03b0eaafb0e1daa1fc010acb4851
danstoyanov <dan.stoyanov@abv.bg>
2021-03-16 22:47:59 +0200

    update

---
+++ C# OOP - Exams/C_Sharp_OOP_Retake_Exam _22_August_2020/01. Structure_Skeleton/EasterRaces/Core/Entities/ChampionshipController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+using EasterRaces.Core.Contracts;
+using EasterRaces.Utilities.Messages;
+using EasterRaces.Repositories.Entities;
+using EasterRaces.Models.Drivers.Entities;
+
+namespace EasterRaces.Core.Entities
+{
+    public class ChampionshipController : IChampionshipController
+    {
+        private readonly CarRepository cars;
+        private readonly DriverRepository drivers;
+        private readonly RaceRepository races;
+
+        public ChampionshipController()
+        {
+            cars = new CarRepository();
+            drivers = new DriverRepository();
+            races = new RaceRepository();
+        }
+
+        public string AddCarToDriver(string driverName, string carModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string AddDriverToRace(string raceName, string driverName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string CreateCar(string type, string model, int horsePower)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string CreateDriver(string driverName)
+        {
+            if (!this.drivers.GetAll().Any(d => d.Name == driverName))
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.DriversExists, driverName));
+            }
+
+            this.drivers.Add(new Driver(driverName));
+
+            return string.Format(OutputMessages.DriverCreated, driverName);
+        }
+
+        public string CreateRace(string name, int laps)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string StartRace(string raceName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
