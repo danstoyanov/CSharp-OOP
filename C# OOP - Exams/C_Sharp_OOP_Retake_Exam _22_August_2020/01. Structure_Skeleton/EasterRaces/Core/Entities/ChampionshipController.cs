67aeb6287b3a407fabecb7ecba4581aaa835bf2d
danstoyanov <dan.stoyanov@abv.bg>
2021-03-27 15:03:04 +0200

    update

--- C# OOP - Exams/C_Sharp_OOP_Retake_Exam _22_August_2020/01. Structure_Skeleton/EasterRaces/Core/Entities/ChampionshipController.cs
+++ C# OOP - Exams/C_Sharp_OOP_Retake_Exam _22_August_2020/01. Structure_Skeleton/EasterRaces/Core/Entities/ChampionshipController.cs
@@ -6,9 +6,8 @@ using System.Collections.Generic;
 using EasterRaces.Core.Contracts;
 using EasterRaces.Utilities.Messages;
 using EasterRaces.Models.Cars.Entities;
-using EasterRaces.Repositories.Entities;
-using EasterRaces.Models.Cars.Contracts;
 using EasterRaces.Models.Races.Entities;
+using EasterRaces.Models.Cars.Contracts;
 using EasterRaces.Models.Races.Contracts;
 using EasterRaces.Models.Drivers.Entities;
 using EasterRaces.Models.Drivers.Contracts;
@@ -17,138 +16,146 @@ namespace EasterRaces.Core.Entities
 {
     public class ChampionshipController : IChampionshipController
     {
-        private readonly CarRepository cars;
-        private readonly DriverRepository drivers;
-        private readonly RaceRepository races;
+        private readonly List<IDriver> drivers;
+        private readonly List<ICar> cars;
+        private readonly List<IRace> races;
 
         public ChampionshipController()
         {
-            cars = new CarRepository();
-            drivers = new DriverRepository();
-            races = new RaceRepository();
+            this.drivers = new List<IDriver>();
+            this.cars = new List<ICar>();
+            this.races = new List<IRace>();
         }
 
         public string AddCarToDriver(string driverName, string carModel)
         {
-
-            if (!this.drivers.GetAll().Any(d => d.Name == driverName))
+            if (!this.drivers.Any(d => d.Name == driverName))
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.DriverNotFound, driverName));
             }
 
-            if (!this.cars.GetAll().Any(c => c.Model == carModel))
+            if (! this.cars.Any(c => c.Model == carModel))
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.CarNotFound, carModel));
             }
 
-            ICar currentCarModel = this.cars.GetAll().FirstOrDefault(c => c.Model == carModel);
-            IDriver currentDriver = this.drivers.GetAll().FirstOrDefault(d => d.Name == driverName);
+            var currentDriver = this.drivers.FirstOrDefault(d => d.Name == driverName);
+            var currentCar = this.cars.FirstOrDefault(c => c.Model == carModel);
 
-            currentDriver.AddCar(currentCarModel);
+            currentDriver.AddCar(currentCar);
 
-            return string.Format(OutputMessages.CarAdded, driverName, carModel);
+            string outputMsg = string.Format(OutputMessages.CarAdded, driverName, carModel);
+
+            return outputMsg.ToString().Trim();
         }
 
         public string AddDriverToRace(string raceName, string driverName)
         {
-            if (!this.races.GetAll().Any(r => r.Name == raceName))
+
+            if (!this.races.All(r => r.Name == raceName))
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.RaceNotFound, raceName));
             }
 
-            if (!this.drivers.GetAll().Any(d => d.Name == driverName))
+            if (!this.drivers.Any(d => d.Name == driverName))
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.DriverNotFound, driverName));
             }
 
-            IRace currentRace = this.races.GetAll().FirstOrDefault(r => r.Name == raceName);
-            IDriver currentDriver = this.drivers.GetAll().FirstOrDefault(d => d.Name == driverName);
+            var currentRace = this.races.FirstOrDefault(r => r.Name == raceName);
+            var currentDriver = this.drivers.FirstOrDefault(d => d.Name == driverName);
 
             currentRace.AddDriver(currentDriver);
 
-            return string.Format(OutputMessages.DriverAdded, driverName, raceName);
+            string outputMsg = string.Format(OutputMessages.DriverAdded, driverName, raceName);
+
+            return outputMsg.ToString().Trim();
         }
 
         public string CreateCar(string type, string model, int horsePower)
         {
-            if (this.cars.GetAll().Any(c => c.Model == model))
+            if (this.cars.Exists(c => c.Model == model))
             {
                 throw new ArgumentException(string.Format(ExceptionMessages.CarExists, model));
             }
 
-            if (type == "Sports")
-            {
-                this.cars.Add(new SportsCar(model, horsePower));
-            }
-            else if (type == "Muscle")
+            string outputMsg = "";
+
+            if (type == "Muscle")
             {
                 this.cars.Add(new MuscleCar(model, horsePower));
+                outputMsg = string.Format(OutputMessages.CarCreated, "MuscleCar", model);
+            }
+            else if (type == "Sports")
+            {
+                this.cars.Add(new SportsCar(model, horsePower));
+                outputMsg = string.Format(OutputMessages.CarCreated, "SportsCar", model);
             }
 
-            return string.Format(OutputMessages.CarCreated, type, model);
+            return outputMsg.ToString().Trim();
         }
 
         public string CreateDriver(string driverName)
         {
-            if (this.drivers.GetAll().Any(d => d.Name == driverName))
+            if (this.drivers.Exists(d => d.Name == driverName))
             {
                 throw new ArgumentException(string.Format(ExceptionMessages.DriversExists, driverName));
             }
 
             this.drivers.Add(new Driver(driverName));
+            string outputMsg = string.Format(OutputMessages.DriverCreated, driverName);
 
-            return string.Format(OutputMessages.DriverCreated, driverName);
+            return outputMsg.ToString().Trim();
         }
 
         public string CreateRace(string name, int laps)
         {
-            if (this.races.GetAll().Any(r => r.Name == name))
+            if (this.races.Any(r => r.Name == name))
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.RaceExists, name));
             }
 
             this.races.Add(new Race(name, laps));
 
-            return string.Format(OutputMessages.RaceCreated, name);
+            string outputMsg = string.Format(OutputMessages.RaceCreated, name);
+
+            return outputMsg.ToString().Trim();
         }
 
         public string StartRace(string raceName)
         {
-            if (!this.races.GetAll().Any(r => r.Name == raceName))
+            if (this.races.Any(r => r.Name != raceName))
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.RaceNotFound, raceName));
             }
 
-            IRace currentRace = this.races.GetAll().FirstOrDefault(r => r.Name == raceName);
+            var currentRace = this.races.FirstOrDefault(r => r.Name == raceName);
 
             if (currentRace.Drivers.Count < 3)
             {
                 throw new InvalidOperationException(string.Format(ExceptionMessages.RaceInvalid, raceName, 3));
             }
 
-            List<IDriver> drivers = currentRace.Drivers
-                .ToList()
-                .OrderByDescending(d => d.Car.CalculateRacePoints(currentRace.Laps))
-                .Take(3)
-                .ToList();
+            var list = new List<IDriver>(currentRace.Drivers);
+            var sb = new StringBuilder();
 
-            StringBuilder sb = new StringBuilder();
+            list.OrderByDescending(d => d.CanParticipate);
 
-            for (int i = 0; i < drivers.Count; i++)
+            for (int i = 0; i < list.Count; i++)
             {
-                IDriver raceDriver = drivers[i];
-
                 if (i == 0)
                 {
-                    sb.AppendLine(string.Format(OutputMessages.DriverFirstPosition, raceDriver.Name, raceName));
+                    sb.AppendLine(string.Format(OutputMessages.DriverFirstPosition, list[i].Name, raceName));
                 }
-                else if (i == 1)
+                
+                if (i == 1)
                 {
-                    sb.AppendLine(string.Format(OutputMessages.DriverSecondPosition, raceDriver.Name, raceName));
+                    sb.AppendLine(string.Format(OutputMessages.DriverSecondPosition, list[i].Name, raceName));
                 }
-                else if (i == 2)
+
+                if (i == 2)
                 {
-                    sb.AppendLine(string.Format(OutputMessages.DriverThirdPosition, raceDriver.Name, raceName));
+                    sb.AppendLine(string.Format(OutputMessages.DriverThirdPosition, list[i].Name, raceName));
                 }
             }
 
