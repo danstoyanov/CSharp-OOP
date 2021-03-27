14658040d891106f163db380f72338310c8f9d8b
danstoyanov <dan.stoyanov@abv.bg>
2021-03-27 15:00:20 +0200

    update

--- C# OOP - Exams/C_Sharp_OOP_Exam_12_December_2020/C_Sharp_OOP_Exam_12_December_2020/Bakery/StartUp.cs
+++ C# OOP - Exams/C_Sharp_OOP_Exam_12_December_2020/C_Sharp_OOP_Exam_12_December_2020/Bakery/StartUp.cs
@@ -7,6 +7,9 @@
         public static void Main(string[] args)
         {
             //Don't forget to comment out the commented code lines in the Engine class!
+
+            var test_Repo = "Test";
+
             var engine = new Engine();
             engine.Run();
         }
